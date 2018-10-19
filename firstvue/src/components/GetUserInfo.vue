d3b8cf784fb64bfd0c3aaceb195512ba92eab1e8
EX-GUOYONG002 <280993186@qq.com>
2018-10-19 10:03:41 +0800

    这是测试提交的

---
+++ firstvue/src/components/GetUserInfo.vue
@@ -0,0 +1,13 @@
+<template>
+    
+</template>
+
+<script>
+    export default {
+        name: "GetUserInfo"
+    }
+</script>
+
+<style scoped>
+
+</style>
--- firstvue/src/router/index.js
+++ firstvue/src/router/index.js
@@ -3,6 +3,8 @@ import Router from 'vue-router'
 
 //懒加载方式，当路由被访问的时候才加载对应组件
 const Login = resolve => require(['@/components/Login'], resolve)
+const GetUserInfo = resolve => require(['@/components/GetUserInfo'], resolve)
+
 
 Vue.use(Router)
 
@@ -12,6 +14,11 @@ export default new Router({
       path: '/',
       name: '登录',
       component: Login
+    },
+    {
+      path: '/user/getUserInfo',
+      name: '登录',
+      component: GetUserInfo
     }
   ]
 })
